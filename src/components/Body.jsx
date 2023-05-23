96d489ec166858cc191f98c7f0ea0887e6b82dfa
Mishael <mishaelclifford1@gmail.com>
2023-05-23 16:54:27 +0100

    Little update

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,7 +4,7 @@ import { Main } from "./Info";
 function Body() {
   return (
     <div>
-      <div className="  bg-gradient-to-r from-blue-500 to-blue-400 h-screen py-7">
+      <div className="  bg-gradient-to-r from-blue-500 to-blue-400 h-screen py-10">
         <Main />
       </div>
     </div>
